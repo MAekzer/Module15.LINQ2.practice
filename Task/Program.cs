306c18c0d00203d38524ba6c607eafd93e0b86ea
MAekzer <gma312@yandex.ru>
2022-10-28 19:17:56 +0300

    Добавьте файлы проекта.

---
+++ Task/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = (from classroom in classes
+                               from student in classroom.Students
+                               select student).ToArray();
+
+            return allStudents;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new();
+        }
+    }
+}
